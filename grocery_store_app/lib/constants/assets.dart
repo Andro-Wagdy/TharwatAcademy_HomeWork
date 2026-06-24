e130d9822b07a1945088fde39d3a65093c904190
Andro-Wagdy <andro.venos55@gmail.com>
2026-06-24 03:41:43 +0300

    added initial assets

---
+++ grocery_store_app/lib/constants/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsCancelIcon
+  /// assets/icons/cancel_icon.svg
+  static const String assetsIconsCancelIcon = "assets/icons/cancel_icon.svg";
+
+  /// Assets for assetsImagesFruits1
+  /// assets/images/fruits_1.png
+  static const String assetsImagesFruits1 = "assets/images/fruits_1.png";
+
+  /// Assets for assetsImagesFruits2
+  /// assets/images/fruits_2.png
+  static const String assetsImagesFruits2 = "assets/images/fruits_2.png";
+}
+
